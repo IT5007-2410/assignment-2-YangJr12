9d5af57d169b70d61b689790288926acc32e2516
杨晨悦 <yangchenyue@etsus-MacBook-Pro.local>
2024-10-10 12:40:08 +0800

    Q2 solved

--- src/App.jsx
+++ src/App.jsx
@@ -98,17 +98,62 @@ class Delete extends React.Component {
   }
 }
 
+
+
 class Homepage extends React.Component {
-	constructor() {
-	super();
-	}
-	render(){
-	return (
-	<div>
-		{/*Q2. Placeholder for Homepage code that shows free seats visually.*/}
-	</div>);
-	}
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+        seats: Array(10).fill().map((_, index) => ({ id: index, isReserved: false })),
+      };
+    }
+  
+    componentDidMount() {
+      const travellersData = [
+        { seatNumber: 0 },
+        { seatNumber: 1 },
+        { seatNumber: 2 },
+        { seatNumber: 3 },
+      ];
+  
+      const reservedSeatNumbers = travellersData.map(traveller => traveller.seatNumber);
+  
+      const updatedSeats = this.state.seats.map(seat => ({
+        ...seat,
+        isReserved: reservedSeatNumbers.includes(seat.id)
+      }));
+  
+      this.setState({ seats: updatedSeats });
+    }
+  
+  
+    render(){
+      const seatStyle = {
+        width: '30px',
+        height: '30px',
+        margin: '5px',
+        backgroundColor: 'orange', // Default color for unreserved seats
+      };
+  
+      const reservedSeatStyle = {
+        ...seatStyle,
+        backgroundColor: 'green', // Color for reserved seats
+      };
+      
+      return (
+      <div>
+        {/*Q2. Placeholder for Homepage code that shows free seats visually.*/}
+        <h2>Welcome to Ticket To Ride!!!</h2>
+        <h2>Seat Availability</h2>
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+            {this.state.seats.map((seat) => (
+              <div key={seat.id} style={seat.isReserved ? reservedSeatStyle : seatStyle} />
+            ))}
+        </div>
+      </div>);
+    }
+  }
+  
 class TicketToRide extends React.Component {
   constructor() {
     super();
@@ -120,9 +165,21 @@ class TicketToRide extends React.Component {
   setSelector(value)
   {
   	/*Q2. Function to set the value of component selector variable based on user's button click.*/
+    this.setState({ selector: value });
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.travellers !== this.state.travellers) {
+      localStorage.setItem('travellers', JSON.stringify(this.state.travellers));
+    }
+  }
+  
   componentDidMount() {
-    this.loadData();
+    const savedTravellers = localStorage.getItem('travellers');
+    if (savedTravellers) {
+      this.setState({ travellers: JSON.parse(savedTravellers) });}
+    else {
+      this.loadData();}
   }
 
   loadData() {
@@ -131,7 +188,7 @@ class TicketToRide extends React.Component {
     }, 500);
   }
 
-  bookTraveller(passenger) {
+  bookTraveller(newTraveller) {
 	    /*Q4. Write code to add a passenger to the traveller state variable.*/
   }
 
@@ -144,7 +201,11 @@ class TicketToRide extends React.Component {
         <h1>Ticket To Ride</h1>
 	<div>
 	    {/*Q2. Code for Navigation bar. Use basic buttons to create a nav bar. Use states to manage selection.*/}
-	</div>
+    <button onClick={() => this.setSelector(1)}>Home</button>
+    <button onClick={() => this.setSelector(2)}>Display Travellers</button>
+    <button onClick={() => this.setSelector(3)}>Add Traveller</button>
+    <button onClick={() => this.setSelector(4)}>Delete Traveller</button>
+  </div>
 	<div>
 		{/*Only one of the below four divisions is rendered based on the button clicked by the user.*/}
 		{/*Q2 and Q6. Code to call Instance that draws Homepage. Homepage shows Visual Representation of free seats.*/}
